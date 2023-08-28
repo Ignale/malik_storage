3c973953410e60ae7225b17b65dfa82cdc4ccbd5
Alex <ignat.lex@gmail.com>
2023-08-28 12:40:58 +0300

    fix

--- googledrive.ts
+++ googledrive.ts
@@ -3,7 +3,7 @@ import { GoogleAuth } from 'google-auth-library'
 
 export default function getGoogleDrive() {
   const auth = new google.auth.GoogleAuth({
-    keyFile: `malikbackups-50592b2b0335.json`,
+    keyFile: `./malikbackups-50592b2b0335.json`,
     scopes: "https://www.googleapis.com/auth/drive",
   });
   const driveService = google.drive({ version: "v3", auth });
